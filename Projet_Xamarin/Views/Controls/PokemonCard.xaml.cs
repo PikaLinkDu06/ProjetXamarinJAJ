b7b3ef89029558d3a636a4ac4eff9cdcceea54f3
JulietteDecobert <54988613+JulietteDecobert@users.noreply.github.com>
2021-04-06 16:19:09 +0200

    PokemonCard in Controls

---
+++ Projet_Xamarin/Views/Controls/PokemonCard.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projet_Xamarin.Views.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PokemonCard : ContentView
+    {
+        public PokemonCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
