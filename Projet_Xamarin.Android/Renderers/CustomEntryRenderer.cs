3a6135d34adefd7e7b4b887d188193900fbe886f
NymePse <arnaud.bijou@gmail.com>
2021-04-10 15:39:18 +0200

    Renderers Android & IOS

---
+++ Projet_Xamarin.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Projet_Xamarin.Droid.Renderers;
+using Projet_Xamarin.Views.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry),
+typeof(CustomEntryRenderer))]
+
+namespace Projet_Xamarin.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        // On cree le Renderer Android pour l'element CustomEntry
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetStroke(2, Android.Graphics.Color.Black);
+                gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
+                Control.SetBackground(gradientDrawable);
+                Control.SetPadding(25, Control.PaddingTop, Control.PaddingRight, Control.PaddingBottom);
+            }
+        }
+    }
+}
---
+++ Projet_Xamarin.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using CoreGraphics;
+using Foundation;
+using Projet_Xamarin.iOS.Renderers;
+using Projet_Xamarin.Views.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace Projet_Xamarin.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        // On cree le Renderer IOS pour l'element CustomEntry
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Layer.CornerRadius = 20;
+                Control.Layer.BorderWidth = 2f;
+                Control.Layer.BorderColor = Color.Black.ToCGColor();
+                Control.Layer.BackgroundColor = Color.LightGray.ToCGColor();
+                Control.LeftView = new UIKit.UIView(new CGRect(0, 0, 10,
+                0));
+                Control.LeftViewMode = UIKit.UITextFieldViewMode.Always;
+            }
+        }
+    }
+}
